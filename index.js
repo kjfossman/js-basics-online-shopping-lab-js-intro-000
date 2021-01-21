e9a02a2195111e25ba9c06d0af1e0c3f5341c8e1
Kyle Fossman <kjfossman@gmail.com>
2021-01-21 16:05:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -60,7 +60,8 @@ function placeOrder(cardNumber) {
   if(cardNumber === ' '){
     return `Sorry, we don't have a credit card on file for you.`
   }else {
+    let cost = total()
     cart.splice(0,cart.length)
-    return `Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`
+    return `Your total cost is ${cost}, which will be charged to the card ${cardNumber}.`
   }
 }
