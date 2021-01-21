3a4fef75c06104ec96f29125184607b0983529c7
Kyle Fossman <kjfossman@gmail.com>
2021-01-21 15:16:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -51,7 +51,6 @@ function removeFromCart(item) {
     for(let i = 0; i < cart.length; i++){
     if(item === cart[i].itemName){
     cart.splice(i,1)
-    return cart
 } }
     return `That item is not in your cart.`
 }
